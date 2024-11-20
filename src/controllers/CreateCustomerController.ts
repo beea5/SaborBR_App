77e8e5c912cce0925df6a935dda1aa377c2a3b3f
beea5 <142271226+beea5@users.noreply.github.com>
2024-11-20 17:23:47 -0300

    4
    criei a função cadastrar do cliente, e ajeitei o insomnia que nao esta retornando os dados

--- src/controllers/CreateCustomerController.ts
+++ src/controllers/CreateCustomerController.ts
@@ -1,13 +1,15 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
+//import { CraeteCustomersServices } from '../services/CraeteCustomersServices'
 import { CraeteCustomersServices } from '../services/CraeteCustomersServices'
-
 class CreateCustomerController{
     async handle(request: FastifyRequest, reply: FastifyReply ){
+        const { name, end, fone, cep, complement, email, senha} = request.body as { name: string, end: string, fone: string, cep: string, complement?: string, email: string, senha: string}
+   
+         const customerService = new CraeteCustomersServices()
 
-        const customerService = new CraeteCustomersServices()
-
-        const customer = await customerService.execute();
+        const customer = await customerService.execute({name, end, fone, cep, complement, email, senha});
         
+        reply.send(customer)
     }
 }
 
